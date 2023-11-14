462e64cd27204bbeb48007e3212bcac51025b70c
zzzyoonnn <dwsrh03@gmail.com>
2023-11-14 15:38:38 +0900

    study operator

---
+++ src/Chapter03/OperatorEx1_1.java
@@ -0,0 +1,13 @@
+package Chapter03;
+
+public class OperatorEx1_1 {
+  public static void main(String[] args) {
+    int i = 5;
+    i++;
+    System.out.println(i);
+    
+    i = 5;
+    ++i;
+    System.out.println(i);
+  }
+}
---
+++ src/Chapter03/OperatorEx4_4.java
@@ -0,0 +1,13 @@
+package Chapter03;
+
+public class OperatorEx4_4 {
+  public static void main(String[] args) {
+    int i = -10;
+    i = +i;
+    System.out.println(i);
+    
+    i = -10;
+    i = -i;
+    System.out.println(i);
+  }
+}
