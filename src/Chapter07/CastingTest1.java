c7bf75f82be6e84ca5335ab9741430966cc2dc09
zzzyoonnn <dwsrh03@gmail.com>
2023-12-15 22:46:09 +0900

    study polymorphism

---
+++ src/Chapter07/CastingTest1.java
@@ -0,0 +1,34 @@
+package Chapter07;
+
+class CastingTest1 {
+  public static void main(String[] args) {
+    Car car = null;
+    FireEngine fe = new FireEngine();
+    FireEngine fe2 = null;
+    
+    fe.water();
+    car = fe;   // car = (Car)fe;에서 형변환이 생략된 형태
+    // car.water();     // Car타입의 참조변수로는 water() 호출 불가능
+    fe2 = (FireEngine)car;      // 자손타입 <- 조상타입
+    fe2.water();
+  }
+}
+
+class Car {
+  String color;
+  int door;
+  
+  void drive() {    // 운전하는 기능
+    System.out.println("drive, Brrr~");
+  }
+  
+  void stop() {     // 멈추는 기능
+    System.out.println("stop!!!");
+  }
+}
+
+class FireEngine extends Car {  // 소방차
+  void water() {    // 물을 뿌리는 기능
+    System.out.println("water!!!");
+  }
+}
---
+++ src/Chapter07/InstanceOfTest.java
@@ -0,0 +1,24 @@
+package Chapter07;
+
+class InstanceOfTest {
+  public static void main(String[] args) {
+    FireEngine fe = new FireEngine();
+    
+    if (fe instanceof FireEngine) {
+      System.out.println("This is a FireEngine instance.");
+    }
+    
+    if (fe instanceof Car) {
+      System.out.println("This is a Car instance.");
+    }
+    
+    if (fe instanceof Object) {
+      System.out.println("This is an Object instance.");
+    }
+    
+    System.out.println(fe.getClass().getName());    // 클래스의 이름 출력
+  }
+}
+
+class Car { }
+class FireEngine extends Car { }
