5cd89e88399fd8b9f5ead2fc4bdd45fd4be52549
zzzyoonnn <dwsrh03@gmail.com>
2023-12-30 23:35:15 +0900

    String_join(), StringJoiner

---
+++ src/Chapter09/StringEx04.java
@@ -0,0 +1,19 @@
+package Chapter09;
+
+import java.util.StringJoiner;
+
+class StringEx04 {
+  public static void main(String[] args) {
+    String animals = "dog, cat, bear";
+    String[] arr = animals.split(", ");
+    
+    System.out.println(String.join("-", arr));
+    
+    StringJoiner sj = new StringJoiner("/", "[", "]");
+    for (String s : arr) {
+      sj.add(s);
+    }
+    
+    System.out.println(sj.toString());
+  }
+}
