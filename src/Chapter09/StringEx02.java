ff6ab7d9d08db4f7694c4c4d1919980a42b936b7
zzzyoonnn <dwsrh03@gmail.com>
2023-12-30 23:28:38 +0900

    String_String literal

---
+++ src/Chapter09/StringEx02.java
@@ -0,0 +1,10 @@
+package Chapter09;
+
+class StringEx02 {
+  public static void main(String[] args) {
+    String s1 = "AAA";
+    String s2 = "AAA";
+    String s3 = "AAA";
+    String s4 = "BBB";
+  }
+}
