54e25f17c355c511097b81798932c9469a946838
zzzyoonnn <dwsrh03@gmail.com>
2024-03-21 13:01:36 +0900

    Functional Interface

---
+++ src/Chapter14/LambdaEx02.java
@@ -0,0 +1,23 @@
+package Chapter14;
+
+@FunctionalInterface
+interface MyFunction {
+  void myMethod();      // public abstract void myMethod();
+}
+
+class LambdaEx02 {
+  public static void main(String[] args) {
+    MyFunction f = () -> {};    // MyFunction f = (MyFunction)(() -> {});
+    Object obj = (MyFunction)(() -> {});    // Object타입으로 형 변환이 생략됨
+    String str = ((Object)(MyFunction)(() -> {})).toString();
+    
+    System.out.println(f);
+    System.out.println(obj);
+    System.out.println(str);
+    
+    //System.out.println(() -> {});         // 에러. 람다식은 Object타입으로 형 변환 불가능
+    System.out.println((MyFunction)(() -> {}));
+    //System.out.println((MyFunction)(() -> {}).toString());      // 에
+    System.out.println(((Object)(MyFunction)(() -> {})).toString());
+  }
+}
