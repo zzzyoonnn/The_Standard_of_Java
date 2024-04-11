a584f93c83ac76f9109cc9afa9f6ed11a11f2919
zzzyoonnn <dwsrh03@gmail.com>
2024-04-11 13:46:54 +0900

    ByteArrayInputStream, ByteArrayOutputStream

---
+++ src/Chapter15/IOEx04.java
@@ -0,0 +1,31 @@
+package Chapter15;
+
+import java.io.*;
+import java.util.Arrays;
+
+class IOEx04 {
+  public static void main(String[] args) {
+    byte[] inSrc  = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    byte[] outSrc = null;
+    byte[] temp = new byte[4];
+    
+    ByteArrayInputStream  input  = null;
+    ByteArrayOutputStream output = null;
+    
+    input  = new ByteArrayInputStream(inSrc);
+    output = new ByteArrayOutputStream();
+    
+    try {
+      while (input.available() > 0) {
+        int len = input.read(temp);     // 읽어 온 데이터의 개수를 반환
+        output.write(temp, 0, len);     // 읽어 온 만큼만 write
+      }
+    } catch (IOException e) {}
+    
+    outSrc = output.toByteArray();
+    
+    System.out.println("Input Source  : " + Arrays.toString(inSrc));
+    System.out.println("temp          : " + Arrays.toString(temp));
+    System.out.println("Output Source : " + Arrays.toString(outSrc));
+  }
+}
