24c95215b21a935f25fe25405e2cca0c004bc5ae
zzzyoonnn <dwsrh03@gmail.com>
2024-04-16 23:06:33 +0900

    DataInputStream, DataOutputStream

---
+++ src/Chapter15/DataInputStreamEx01.java
@@ -0,0 +1,20 @@
+package Chapter15;
+
+import java.io.*;
+
+class DataInputStreamEx01 {
+  public static void main(String[] args) {
+    try {
+      FileInputStream fis = new FileInputStream("sample.dat");
+      DataInputStream dis = new DataInputStream(fis);
+      
+      System.out.println(dis.readInt());
+      System.out.println(dis.readFloat());
+      System.out.println(dis.readBoolean());
+      
+      dis.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/Chapter15/DataOutputStreamEx01.java
@@ -0,0 +1,22 @@
+package Chapter15;
+
+import java.io.*;
+
+class DataOutputStreamEx01 {
+  public static void main(String[] args) {
+    FileOutputStream fos = null;
+    DataOutputStream dos = null;
+    
+    try {
+      fos = new FileOutputStream("sample.dat");
+      dos = new DataOutputStream(fos);
+      dos.writeInt(10);
+      dos.writeFloat(20.0f);
+      dos.writeBoolean(true);
+      
+      dos.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
