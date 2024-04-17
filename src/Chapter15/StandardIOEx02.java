adc4456f059049d511689b327a277c36e3615e77
zzzyoonnn <dwsrh03@gmail.com>
2024-04-17 22:12:25 +0900

    표준 입출력의 대상 변경

---
+++ src/Chapter15/StandardIOEx02.java
@@ -0,0 +1,8 @@
+package Chapter15;
+
+class StandardIOEx02 {
+  public static void main(String[] args) {
+    System.out.println("out : Hello World!");
+    System.out.println("err : Hello World!");
+  }
+}
